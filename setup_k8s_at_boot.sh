f79c1d0ba1f61849271a66c0b6ed91b5b5843657
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-04-09 07:45:44 +0200

    Check kubelab pod status

--- setup_k8s_at_boot.sh
+++ setup_k8s_at_boot.sh
@@ -160,6 +160,11 @@ kubectl -n kubelab create configmap kube-configmap --from-file=/home/ubuntu/.kub
 
 kubectl create -f /root/github.com/kubelab/kubelab.yaml
 
+kubectl -n kubelab get cm
+kubectl -n kubelab get pods | grep " Running " || sleep 10
+kubectl -n kubelab get pods | grep " Running " || sleep 10
+kubectl -n kubelab get pods | grep " Running " || sleep 10
+
 EOF
 
     chmod +x /tmp/kubelab.sh
