555d9a3d0ecba0b3e374aba2871f075d3f7ee845
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-04-19 09:33:54 +0200

    Add short Docker version

--- setup_k8s_at_boot.sh
+++ setup_k8s_at_boot.sh
@@ -152,7 +152,8 @@ START_DOCKER_plus() {
 
     groupadd docker
     usermod -aG docker ubuntu
-    { echo "ubuntu: docker ps"; sudo -u ubuntu docker ps; } | SECTION_LOG
+    #{ echo "ubuntu: docker ps"; sudo -u ubuntu docker ps; } | SECTION_LOG
+    docker version -f "Docker Version Client={{.Client.Version}} Server={{.Server.Version}}"
     echo "ubuntu: docker version"; sudo -i docker version
     # newgrp docker # In shell allow immediate joining of group / use of docker
 }
