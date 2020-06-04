6ad86d9fb8b23a5c1a7cf9d7b3dbce0a7f533e24
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-06-04 09:28:36 +0200

    Add ability to rerun the installer
    ... and saving the initial log file

---
+++ RERUN_INSTALL.sh
@@ -0,0 +1,8 @@
+
+
+[ ! -f /root/tmp/user-data.op.initial] && mv /root/tmp/user-data.op /root/tmp/user-data.op.initial
+
+bash -x /root/tmp/instance/user-data.txt > /root/tmp/user-data.op.rerun 2>&1
+
+
+
