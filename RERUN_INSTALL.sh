9d40855892e6a8d4d497fe9b77d743ae9267ac4c
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-07-11 11:44:19 +0200

    Added -o option to rerun install as the event owner

--- RERUN_INSTALL.sh
+++ RERUN_INSTALL.sh
@@ -5,6 +5,10 @@ USER_DATA_LOG=/root/tmp/user-data.op
 [   -f ${USER_DATA_LOG}         ] && cp -a $USER_DATA_LOG ${USER_DATA_LOG}.$(date +'%Y-%m-%d_%H-%M-%S')
 [ ! -f ${USER_DATA_LOG}.initial ] && [ -f $USER_DATA_LOG ] && mv $USER_DATA_LOG ${USER_DATA_LOG}.initial
 
+USER_IS_OWNER=""
+[ "$1" = "-o" ]      && USER_IS_OWNER="1"
+[ "$1" = "--owner" ] && USER_IS_OWNER="1"
+
 bash -x /root/tmp/instance/user-data.txt > ${USER_DATA_LOG}.rerun 2>&1
 
 while ! kubectl get nodes; do
